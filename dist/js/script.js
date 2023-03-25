8f378ab9def54f8ce71627a6f84bf99b854220d9
dreeCTS <dree.catuas@gmail.com>
2023-03-25 12:08:04 +0700

    update dark mode etc.

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden)");
   }
 };
 
@@ -18,3 +23,36 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("menu-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik di luar Menu
+window.addEventListener("click", function (e) {
+  if (e.target != menu && e.target != navMenu) {
+    menu.classList.remove("menu-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
